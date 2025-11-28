a5c247a4611ba74e5d3da983752cc69fdc80f1ca
yusufsenturk <yusufsengs8@gmail.com>
2025-11-28 23:31:23 +0300

    Add Splash Screen and update App Name to SkyCast

---
+++ app/src/main/java/com/example/wetherapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.wetherapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Wait for 2 seconds and then open MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish() // Close SplashActivity so user can't go back to it
+        }, 1300)
+    }
+}
